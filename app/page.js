b1e7daba6e3a358b286352e11e4ad365c9103b4f
samueloliveira02 <samueloliveiragordin@gmail.com>
2024-10-18 01:34:58 -0300

    teste

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="m-0 p-0 box-border">
       <div className="h-20 w-full bg-slate-900 flex justify-around flex-row p-4 items-center">
         <h1 className="font-bold text-2xl italic">Cruelty</h1>
+        <h2>black nigga</h2>
         <ul className="flex justify-between gap-6">
           <li className="font-semibold hover:text-green-500 cursor-pointer">Inicio</li>
           <li className="font-semibold hover:text-green-500 cursor-pointer">Drops</li>
