4323719f58ff7fb5392c0644917bcf0d851c6791
devnkz <vieiranyckolas1@gmail.com>
2024-10-25 12:58:23 -0300

    add animations da lib Frame-motion

--- app/page.js
+++ app/page.js
@@ -1,9 +1,27 @@
+"use client"
+
 import { Cinzel } from "next/font/google";
 import { Header } from "./components/header/page";
 import { Images } from "./components/img/page";
 import { CardProduto } from "./components/cardProdutos/page";
 import { Footer } from "./components/footer/page";
-import { Sobre } from "./screen/sobre/page";
+import { motion } from "framer-motion";
+
+const cardVariants = {
+  offscreen: {
+    y: 100,
+    opacity: 0
+  },
+  onscreen: {
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      bounce: 0.2,
+      duration: 0.9
+    }
+  }
+};
 
 
 const FontCinzel = Cinzel({
@@ -14,7 +32,7 @@ const FontCinzel = Cinzel({
 export default function Home() {
   return (
     <div id="Home" className="bg-zinc-900">
-      <Header LinkNextVoltar={"/"} idHome={"#Home"} idDrop={"#Drop"} idFooter={"#Footer"} LinkNext={"/screen/sobre"}/>
+      <Header LinkNextVoltar={"/"} idHome={"#Home"} idDrop={"#Drop"} idFooter={"#Footer"} LinkNext={"/screen/sobre"} />
       <article className="h-56 w-full flex items-end justify-center gap-4">
         <div className={FontCinzel.className}>
           <h1 className="text-5xl text-green-600">Seja<span className="text-white"> Cruel</span></h1>
@@ -26,13 +44,25 @@ export default function Home() {
         <Images src={"/img3.png"} />
         <Images src={"/img4.png"} />
       </section>
-      <div className="w-full flex items-center justify-center">
-        <section id="Drop" className="mt-24 w-7/12 flex flex-row items-center justify-evenly mb-5 flex-wrap gap-24">
-          <CardProduto src={"/camisa.png"} valor_produto={"165,00"} nome_produto={"Camisa Plague War"} />
-          <CardProduto src={"/calca.png"} valor_produto={"389,00"} nome_produto={"Toxic Denim Double Knee"} />
-          <CardProduto src={"/shape1.png"} valor_produto={"299,00"} nome_produto={"Shape Faith White"} />
-          <CardProduto src={"/shape2.png"} valor_produto={"299,00"} nome_produto={"Shape Faith black"} />
-        </section>
+      <div className="w-full flex items-center justify-center flex-col mt-24 gap-12">
+        <motion.section id="Drop"
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.2 }}>
+          <motion.div variants={cardVariants} className="flex flex-row gap-36">
+            <CardProduto src={"/camisa.png"} valor_produto={"165,00"} nome_produto={"Camisa Plague War"} />
+            <CardProduto src={"/calca.png"} valor_produto={"389,00"} nome_produto={"Toxic Denim Double Knee"} />
+          </motion.div>
+        </motion.section>
+        <motion.section id="Drop"
+          initial="offscreen"
+          whileInView="onscreen"
+          viewport={{ once: true, amount: 0.5 }}>
+          <motion.div variants={cardVariants} className="flex flex-row gap-36">
+            <CardProduto src={"/shape1.png"} valor_produto={"299,00"} nome_produto={"Shape Faith White"} />
+            <CardProduto src={"/shape2.png"} valor_produto={"299,00"} nome_produto={"Shape Faith black"} />
+          </motion.div>
+        </motion.section>
       </div>
       <footer id="Footer">
         <Footer />
--- app/screen/sobre/page.js
+++ app/screen/sobre/page.js
@@ -1,7 +1,28 @@
+"use client"
+
 import { Footer } from "@/app/components/footer/page";
 import { Header } from "@/app/components/header/page";
 import { Cinzel, Montserrat } from "next/font/google";
 import Image from "next/image";
+import { motion } from "framer-motion";
+
+const cardVariants = {
+    offscreen: {
+        y: 100,
+        opacity: 0
+    },
+    onscreen: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            type: "spring",
+            bounce: 0.2,
+            duration: 0.9
+        }
+    }
+};
+
+
 const FontCinzel = Cinzel({
     weight: ['400'],
     subsets: ['latin'],
@@ -13,33 +34,53 @@ const FontMontserrat = Montserrat({
 })
 
 export default function Sobre() {
+
+
+
     return (
         <main className="h-screen w-full bg-zinc-900 flex flex-col">
-            <Header LinkNextVoltar={"/"} idHome={"..."} idDrop={"..."} idFooter={"..."} LinkNext={"/screen/sobre"}/>
+            <Header LinkNextVoltar={"/"} idHome={"..."} idDrop={"..."} idFooter={"..."} LinkNext={"/screen/sobre"} />
             <section className="flex h-screen items-center justify-center flex-col gap-12">
-                <div className="flex flex-row gap-12">
-                    <h1 className={`text-white text-5xl ${FontMontserrat.className}`}>
-                        A EMPRESA <span className={`text-green-600  text-6xl ${FontCinzel.className}`}>Cruelty</span></h1>
-                    <Image
-                        className="object-contain rounded-2xl"
-                        src="/logoCRUEL.jpg"
-                        width={50}
-                        height={50}
-                    />
-                </div>
-                <article className={`text-white text-xl mt-4 text-justify ${FontMontserrat.className} w-2/4`}>
-                    <p>
-                        Criada em 2022, a Cruel se destacou por ser uma das marcas pioneiras a trazer a clássica calça jeans baggy de volta ao Brasil.
-                        Além de toda a exclusividade das peças, a Cruel prima pela qualidade excepcional de seus produtos.
-                        Cada peça  é cuidadosamente projetada e confeccionada para atender aos mais altos padrões de qualidade do mundo.
-                        Nossa  matéria-prima é selecionada com rigor. Modelagem e acabamentos perfeitos, garantem um produto final que se
-                        destaca não somente pela beleza, mas também pela sua resistência e durabilidade.
-                        O mundo é Cruel!
-                    </p>
-                </article>
+                <motion.section id="Drop"
+                    initial="offscreen"
+                    whileInView="onscreen"
+                    viewport={{ once: true, amount: 0.2 }}>
+                    <motion.div variants={cardVariants}>
+                        <div className="flex flex-row gap-12">
+                            <h1 className={`text-white text-5xl ${FontMontserrat.className}`}>
+                                A EMPRESA <span className={`text-green-600  text-6xl ${FontCinzel.className}`}>Cruelty</span></h1>
+                            <Image
+                                className="object-contain rounded-2xl"
+                                src="/logoCRUEL.jpg"
+                                width={50}
+                                height={50}
+                            />
+                        </div>
+                    </motion.div>
+                </motion.section>
+                <motion.section id="Drop"
+                    initial="offscreen"
+                    whileInView="onscreen"
+                    viewport={{ once: true, amount: 0.4 }}
+                    className={`text-white text-xl mt-4 text-justify ${FontMontserrat.className} w-2/4`}
+                >
+
+                    <motion.div variants={cardVariants}>
+                        <article >
+                            <p>
+                                Criada em 2022, a Cruel se destacou por ser uma das marcas pioneiras a trazer a clássica calça jeans baggy de volta ao Brasil.
+                                Além de toda a exclusividade das peças, a Cruel prima pela qualidade excepcional de seus produtos.
+                                Cada peça  é cuidadosamente projetada e confeccionada para atender aos mais altos padrões de qualidade do mundo.
+                                Nossa  matéria-prima é selecionada com rigor. Modelagem e acabamentos perfeitos, garantem um produto final que se
+                                destaca não somente pela beleza, mas também pela sua resistência e durabilidade.
+                                O mundo é Cruel!
+                            </p>
+                        </article>
+                    </motion.div>
+                </motion.section>
             </section>
             <footer>
-            <Footer />
+                <Footer />
             </footer>
         </main>
     )
