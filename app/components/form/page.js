ea85a812573fac23f11023ff58a015a8711d9d64
devnkz <vieiranyckolas1@gmail.com>
2024-10-24 22:58:55 -0300

    add border no login

--- app/components/form/page.js
+++ app/components/form/page.js
@@ -13,7 +13,7 @@ export function Form() {
   return (
     <div className={FontMontserrat.className}>
       <div className="w-full h-screen bg-transparent flex items-center justify-center">
-        <Tabs defaultValue="account" className="w-[400px] flex items-center justify-center flex-col bg-black p-12 rounded-lg">
+        <Tabs defaultValue="account" className="w-[400px] border-green-700 border-2 flex items-center justify-center flex-col bg-black p-12 rounded-lg">
           <TabsList className="bg-zinc-800 text-white w-3/4 justify-between">
             <TabsTrigger className="w-2/4" value="account">Entrar</TabsTrigger>
             <TabsTrigger className="w-2/4" value="password">Criar</TabsTrigger>
--- app/components/img/page.js
+++ app/components/img/page.js
@@ -3,7 +3,7 @@ import Image from "next/image"
 export function Images({ src }){
     return(
         <Image
-          className="object-contain rounded-lg hover:scale-110 transition duration-120"
+          className="object-contain rounded-lg transition duration-120"
           src={src}
           width={400}
           height={400}
